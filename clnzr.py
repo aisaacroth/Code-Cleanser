2d1f8eed5c8a72bd64ff018ca4a9f36a2da68525
aisaacroth <aisaacroth@gmail.com>
2014-09-08 09:51:08 -0400

    Ran pep8 and fixed style issues

--- clnzr.py
+++ clnzr.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 import sys
+''' A simple script to remove access whitespace at the end
+of each line in a file.
+
+Author: Alexander Roth
+Date:   2014-09-07
+'''
+
 
 def clean(filename):
     origin_list = retrieve_lines_from_file(filename)
     clean_list = reformat_lines(origin_list)
     write_clean_file(filename, clean_list)
 
+
 def print_arguments():
     print("python {0} <filename>".format(sys.argv[0]))
+
 
 def retrieve_lines_from_file(filename):
     origin_file = open(filename, 'r')
@@ -15,22 +24,27 @@ def retrieve_lines_from_file(filename):
     origin_file.close()
     return origin_list
 
+
 def reformat_lines(origin_list):
     clean_list = remove_trailing_whitespace(origin_list)
     complete_list = append_newline(clean_list)
     return complete_list
 
+
 def remove_trailing_whitespace(origin_list):
     return [line.rstrip() for line in origin_list]
 
+
 def append_newline(clean_list):
     return [line + "\n" for line in clean_list]
+
 
 def write_clean_file(filename, complete_list):
     clean_file = open(filename, 'w')
     for line in complete_list:
         clean_file.write("%s" % line)
     clean_file.close()
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
