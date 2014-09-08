90eeedd95bc645808f155e83ac95e60ef331c554
aisaacroth <aisaacroth@gmail.com>
2014-09-07 23:50:22 -0400

    Working on clnzr

--- clnzr.py
+++ clnzr.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 import sys
 
-if __name__ == "__main__":
+def print_arguments():
+	print("python {0} <filename>".format(sys.argv[0]))
+
+def main(filename):
 	pass
+
+if __name__ == "__main__":
+	if len(sys.argv) == 2:
+		main(sys.argv[1])
+	else:
+		print_arguments()
