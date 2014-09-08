91208865220b94a4031e1502094641b75f1b643b
aisaacroth <aisaacroth@gmail.com>
2014-09-08 09:05:01 -0400

    Added some more unitttests

--- testClnzr.py
+++ testClnzr.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/b)in/env python
 import unittest
 import clnzr
 
@@ -6,14 +6,33 @@ class TestClnzr(unittest.TestCase):
 
     def setUp(self):
         self.test_file = clnzr.retrieve_file('test.txt')
+        self.test_list = clnzr.get_lines(self.test_file)
+        self.test_list = clnzr.remove_trailing_whitespace(self.test_list)
+
 
     def test_retrieve_file(self):
         self.assertIsNotNone(self.test_file)
 
     def test_getlines(self):
-        self.test_list = clnzr.get_lines(self.test_file)
         self.assertIsNotNone(self.test_list)
-        
+
+    def test_remove_trailing_whitespace(self):
+        self.assertNotIn("\n", self.test_list[0])
+
+    def test_write_clean_file(self):
+        self.test_list = clnzr.retrieve_lines_from_file('test.txt')
+        self.clean_list = clnzr.reformat_lines(self.test_list)
+        clnzr.write_clean_file('test.txt', self.clean_list)
+
+    def test_retrieve_lines_from_file(self):
+        self.test_list = clnzr.retrieve_lines_from_file('test.txt')
+        self.assertIsNotNone(self.test_list)
+
+    def test_reformat_lines(self):
+        self.test_list = clnzr.retrieve_lines_from_file('test.txt')
+        self.clean_list = clnzr.reformat_lines(self.test_list)
+        self.assertIn('\n', self.clean_list[0])
+
     def tearDown(self):
         self.test_file.close()
 
