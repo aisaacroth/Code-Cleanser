7577f84be4996e0498fff9bcaa5c913cf6c2ce95
aisaacroth <aisaacroth@gmail.com>
2014-09-08 00:21:39 -0400

    Working on clnzr functionality

--- clnzr.py
+++ clnzr.py
@@ -2,13 +2,21 @@
 import sys
 
 def print_arguments():
-	print("python {0} <filename>".format(sys.argv[0]))
+    print("python {0} <filename>".format(sys.argv[0]))
+
+def retrieve_file(filename):
+    origin_file = open(filename, 'r')
+    return origin_file
+
+def get_lines(origin_file):
+    origin_list = origin_file.readlines()
+    return origin_list
 
 def main(filename):
-	pass
+    pass
 
 if __name__ == "__main__":
-	if len(sys.argv) == 2:
-		main(sys.argv[1])
-	else:
-		print_arguments()
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        print_arguments()
